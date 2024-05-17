6a04e47ea76f6d52242094381c5fc5fea013e895
junior451 <kwabenaedwards@gmail.com>
2024-05-17 08:49:44 +0100

    implement backend api and login functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+## A todoapp built using react and python django 
---
+++ frontend/src/components/signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Signup(){
+  return (
+    <div classname="App">
+      Signup
+    </div>
+  );
+}
+
+export default Signup;
